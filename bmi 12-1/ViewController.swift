3d8f39ae55afdbda23fb0909da5bb04bfded1b39
willis18 <willis18@naver.com>
2021-02-10 08:25:58 +0900

    Initial Commit

---
+++ bmi 12-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bmi 12-1
+//
+//  Created by 김송현 on 2021/02/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
