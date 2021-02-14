bdd2785abdde1b8a6d08b871f9e34e75a1b2148b
willis18 <willis18@naver.com>
2021-02-15 00:00:20 +0900

    12-8까지함

--- bmi 12-1/ViewController.swift
+++ bmi 12-1/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtHeight: UITextField!
+    @IBOutlet var txtWeight: UITextField!
+    @IBOutlet var lblResult: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func calcBmi(_ sender: UIButton) {
+    }
+    
 
 }
 
