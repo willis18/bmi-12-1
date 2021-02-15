450b541eaceaeae4051cad0492a1fe09b69f7977
willis18 <willis18@naver.com>
2021-02-15 18:07:04 +0900

    13-3 까지 완료

--- bmi 12-1/ViewController.swift
+++ bmi 12-1/ViewController.swift
@@ -16,9 +16,46 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func calcBmi(_ sender: UIButton) {
+    @IBAction func calcBmi(_ sender: UIButton)
+    {
+        if txtHeight.text == "" || txtWeight.text == ""
+        {
+            print("Input Error")
+            lblResult.text = "키와 체중을 입력하세요!"
+            return
+        }
+        else
+        {
+            let height = Double(txtHeight.text!)!
+            let weight = Double(txtWeight.text!)!
+//            print(height, weight)
+            
+            let bmi = weight / (height * height * 0.0001)
+            let shortenedBmi = String(format:
+                                        "%.1f",bmi)
+            var body = ""
+            if bmi >= 40
+            {
+                body = "3단계 미만"
+            }
+            else if bmi >= 30 && bmi < 40
+            {
+                body = "2단계 미만"
+            }
+            else if bmi >= 25 && bmi < 30
+            {
+                body = "1단계 미만"
+            }
+            else if bmi >= 18.5 && bmi < 25
+            {
+                body = "정상"
+            }
+            else
+            {
+                body = "저체중"
+            }
+//            print("BMI : \(shortenedBmi), 판정 : \(body)")
+            lblResult.text = "BMI : \(shortenedBmi), 판정 : \(body)"
+        }
     }
-    
-
 }
-
