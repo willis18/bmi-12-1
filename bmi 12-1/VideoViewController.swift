4a428c876a02ee826d4290f715a12e70f0696587
willis18 <willis18@naver.com>
2021-02-17 23:17:00 +0900

    13-18 까지 완료

---
+++ bmi 12-1/VideoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VideoViewController.swift
+//  bmi 12-1
+//
+//  Created by 김송현 on 2021/02/17.
+//
+
+import UIKit
+import AVKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
